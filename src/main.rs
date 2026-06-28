3b85378a6a658e010b56d9a01012c80837cf810f
JKSoper-ctrl <jamessoper300@gmail.com>
2026-06-28 14:34:36 +0100

    feat: now has median word length

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,11 @@ fn main() {
 
     let mut most_frequent_words: Vec<&str> = Vec::new();
     let mut most_frequent_word_count: u32 = 0;
+    let mut word_lengths: Vec<usize> = Vec::new();
 
     for word in text.split_whitespace() {
+        word_lengths.push(word.len());
+        
         let count = word_map.entry(word).or_insert(0);
         *count += 1;
 
@@ -28,15 +31,13 @@ fn main() {
             most_frequent_word_count = *count;
         }
     }
-            
-            
-            
-            
+
+    let median_word_length = get_median(&mut word_lengths);
 
     dbg!(word_map);
-
     dbg!(most_frequent_words);
     dbg!(most_frequent_word_count);
+    dbg!(median_word_length);
 
 
     println!("{text}");
@@ -56,3 +57,25 @@ fn get_input(instruction: &str) -> String {
 
     input_text
 }
+
+fn get_median(numbers: &mut [usize]) -> f64 {
+    numbers.sort_unstable();
+    
+    let numbers_length = numbers.len();
+    let numbers_mid = numbers_length / 2;
+    
+    if numbers_length.is_multiple_of(2) { //if even number of elements
+        ((numbers[numbers_mid - 1] + numbers[numbers_mid]) as f64) / 2.0 
+    } else {
+        numbers[numbers_mid] as f64
+    }
+}
+
+/*
+    word_lengths.sort_unstable();
+    let word_lengths_length = word_lengths;
+    let word_lengths_mid = word_lengths / 2;
+
+    if word_lengths_length
+            
+*/
